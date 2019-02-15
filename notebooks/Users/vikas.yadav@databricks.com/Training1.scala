5b587b6441e9bccc4803e1204f94e3572bf46737
VikYadav <47671794+VikYadav@users.noreply.github.com>
2019-02-15 18:35:50 +0100

    first commit

---
+++ notebooks/Users/vikas.yadav@databricks.com/Training1.scala
@@ -0,0 +1,31 @@
+// Databricks notebook source
+//dbutils.fs.mkdirs("dbfs:/FileStore/TestVik")
+
+display(dbutils.fs.ls("dbfs:/FileStore/TestVik"))
+
+// COMMAND ----------
+
+display(dbutils.fs.ls("dbfs:/FileStore/tables/first.txt"))
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
+val partition_path = dbutils.fs.ls("dbfs:/FileStore/TestVik/").filter(r => r.name.endsWith(".txt"))(0).path
+dbutils.fs.cp(partition_path,"dbfs:/FileStore/tables/outputFile.csv")
+
+
+// COMMAND ----------
+
+// MAGIC %sh #!bin/bash
+// MAGIC 
+// MAGIC apt-get install python-rdkit librdkit1 rdkit-data
+
+// COMMAND ----------
+
+
+
+// COMMAND ----------
+
